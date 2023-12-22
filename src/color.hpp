a3a76d66af172d8be0f7f38d1be71925215071a2
Julian Lamas-Rodriguez <julian@lamasrodriguez.com>
2023-12-22 23:29:46 +0100

    Apply gamma correction

--- src/color.hpp
+++ src/color.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include <iostream>
 
 #include "interval.hpp"
@@ -57,11 +59,17 @@ inline auto operator<<(std::ostream &t_out, const pixel<T> t_pixel) -> std::ostr
     // Divide the color by the number of samples
     const auto norm_color = static_cast<color<T>>(t_pixel.pixel_color / static_cast<T>(t_pixel.samples_per_pixel));
 
+    // Transform color from linear to gamma space
+    constexpr auto linear_to_gamma = [](const color<T> linear_color) -> color<T> {
+        return {std::sqrt(linear_color.r()), std::sqrt(linear_color.g()), std::sqrt(linear_color.b())};
+    };
+    const auto gamma_color = linear_to_gamma(norm_color);
+
     // Write the translated [0,255] value of each color component.
     constexpr auto intensity = interval<T>{0.000, 0.999};
     t_out 
-        << static_cast<int>(256 * intensity.clamp(norm_color.r())) << ' '
-        << static_cast<int>(256 * intensity.clamp(norm_color.g())) << ' '
-        << static_cast<int>(256 * intensity.clamp(norm_color.b())) << '\n';
+        << static_cast<int>(256 * intensity.clamp(gamma_color.r())) << ' '
+        << static_cast<int>(256 * intensity.clamp(gamma_color.g())) << ' '
+        << static_cast<int>(256 * intensity.clamp(gamma_color.b())) << '\n';
     return t_out;
 }
